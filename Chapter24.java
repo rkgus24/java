cb7bd7cd48de6966dea8acb87db7920a3c59b849
가현 <102145605+rkgus24@users.noreply.github.com>
2022-09-24 20:15:45 +0900

    Update Chapter24.java

---
+++ Chapter24.java
@@ -0,0 +1,16 @@
+package chapter24;
+//**toString() 메서드
+//java.lang.Object 클래스는 모든 클래스의 최상위 클래스이다
+//모든 클래스는 Object클래스를 상속 받는다
+//Object 클래스는 자동으로 import되어 있다
+//모든 틀래스는 Object클래스의 메서드를 사용할 수 있고, 일부 메서드는 오버라이드해서 사용할 수 있다
+///**toString() 메서드 : 변수(객체)의 정보를 Sting(문자열)형으로 변환해서 표시해준다
+//오버라이드(메서드 고쳐서 사용)하여 사용할 수 있다
+public class Poly_2 {
+
+   public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+   }
+
+}
